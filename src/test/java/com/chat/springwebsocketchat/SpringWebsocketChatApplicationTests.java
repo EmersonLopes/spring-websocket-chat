5a292f47dc5875471e2e96593a5f17d33e0e70e5
EmersonLopes <emersonsilvalopes1@gmail.com>
2022-04-15 19:26:56 -0300

    first commit

---
+++ src/test/java/com/chat/springwebsocketchat/SpringWebsocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.springwebsocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
